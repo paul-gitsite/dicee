91a09186dcd5fd0a1fcea42739ddd9924a372269
paul-gitsite <paulraj2218@gmail.com>
2023-11-14 13:40:38 +0530

    third commit

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; 
 
-var randomImageSource = "images/" + randomDiceImage; 
+var randomImageSource = "./public/images/" + randomDiceImage; 
 
 var image1 = document.querySelectorAll("img")[0];
 
